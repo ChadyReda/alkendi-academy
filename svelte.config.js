179594872195540ee53912ccf897292e44a66aa0
Chady reda <chadyxreda@gmail.com>
2025-01-05 13:18:06 -0800

    ok bro im done

--- svelte.config.js
+++ svelte.config.js
@@ -5,14 +5,9 @@ const dev = process.argv.includes("dev")
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
